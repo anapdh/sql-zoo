6d239f06c17aeec683f2888ea0c34fef22e2e7e3
anapdh <ana_d_hubner@hotmail.com>
2021-01-06 16:48:40 -0300

    add practice and quizz 3

--- .sql
+++ .sql
@@ -172,4 +172,140 @@ Brazil
 Colombia
 
 
-===========
+=========== SELECT from Nobel Tutorial ===========
+This tutorial is concerned with a table of Nobel prize winners:
+
+1.
+SELECT yr, subject, winner
+  FROM nobel
+ WHERE yr = 1950
+
+ 2.
+ SELECT winner
+  FROM nobel
+ WHERE yr = 1962
+   AND subject = 'Literature'
+
+3.
+SELECT yr, subject
+  FROM nobel
+WHERE winner = 'Albert Einstein'
+
+4.
+SELECT winner
+  FROM nobel
+WHERE subject = 'Peace' AND yr >= 2000
+
+5.
+SELECT yr, subject, winner
+  FROM nobel
+WHERE subject = 'Literature' AND yr BETWEEN 1980 AND 1989
+
+6.
+SELECT * FROM nobel
+ WHERE winner IN ('Theodore Roosevelt',
+                  'Woodrow Wilson',
+                  'Jimmy Carter',
+                  'Barack Obama')
+
+7.
+SELECT winner
+ FROM nobel
+WHERE winner LIKE 'John%'
+
+8.
+SELECT * FROM nobel
+WHERE subject = 'Physics' AND yr = 1980 OR subject = 'Chemistry' AND yr = 1984
+
+9.
+SELECT * FROM nobel
+WHERE yr = 1980 AND subject != 'Chemistry' AND subject != 'Medicine'
+
+10.
+SELECT * FROM nobel
+WHERE (subject = 'Medicine' AND yr < 1910) OR (subject = 'Literature' AND yr >= 2004)
+
+11.
+SELECT * FROM nobel
+WHERE winner = 'PETER GRÜNBERG'
+
+12.
+SELECT * FROM nobel
+WHERE winner = 'EUGENE O''NEILL'
+
+13.
+SELECT winner, yr, subject
+  FROM nobel
+WHERE winner LIKE 'Sir%' ORDER BY yr DESC, winner ASC
+
+14.
+SELECT winner, subject
+  FROM nobel
+ WHERE yr = 1984
+ ORDER BY subject IN ('Physics','Chemistry'), subject, winner
+
+ QUIZZ 3
+
+ 1.
+ SELECT winner FROM nobel
+ WHERE winner LIKE 'C%' AND winner LIKE '%n'
+
+ 2.
+ SELECT COUNT(subject) FROM nobel
+ WHERE subject = 'Chemistry'
+   AND yr BETWEEN 1950 and 1960
+
+3.
+SELECT COUNT(DISTINCT yr) FROM nobel
+ WHERE yr NOT IN (SELECT DISTINCT yr FROM nobel WHERE subject = 'Medicine')
+
+4.
+Medicine	Sir John Eccles
+Medicine	Sir Frank Macfarlane Burnet
+
+5.
+SELECT yr FROM nobel
+ WHERE yr NOT IN(SELECT yr 
+                   FROM nobel
+                 WHERE subject IN ('Chemistry','Physics'))
+
+6.
+SELECT DISTINCT yr
+  FROM nobel
+ WHERE subject='Medicine' 
+   AND yr NOT IN(SELECT yr FROM nobel 
+                  WHERE subject='Literature')
+   AND yr NOT IN (SELECT yr FROM nobel
+                   WHERE subject='Peace')
+
+7.
+Chemistry	1
+Literature	1
+Medicine	2
+Peace	1
+Physics	1
+
+
+
+=========== SELECT within SELECT Tutorial ===========
+This tutorial looks at how we can use SELECT statements within SELECT statements to perform more complex queries.
+
+1.
+SELECT name FROM world
+  WHERE population >
+     (SELECT population FROM world
+      WHERE name='Russia')
+
+2.
+SELECT name FROM world
+WHERE continent = 'Europe' AND gdp / population >
+(SELECT gdp / population FROM world WHERE name = 'United Kingdom')
+
+3.
+SELECT name, continent FROM world
+WHERE continent IN 
+(SELECT continent FROM world WHERE name IN ('Argentina', 'Australia')) ORDER BY name ASC;
+
+4.
+
+
