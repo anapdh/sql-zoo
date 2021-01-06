128f79a5d5e863ec0313f2d301349b0ee4bd8dd3
anapdh <ana_d_hubner@hotmail.com>
2021-01-06 17:18:10 -0300

    add practice and quizz 4

--- .sql
+++ .sql
@@ -307,5 +307,44 @@ WHERE continent IN
 (SELECT continent FROM world WHERE name IN ('Argentina', 'Australia')) ORDER BY name ASC;
 
 4.
+SELECT name, population FROM world
+WHERE population >
+(SELECT name FROM world WHERE population > 'Canada' AND population < 'Poland')
 
+5.
+SELECT name, CONCAT(ROUND(population/(SELECT population FROM world
+WHERE name = 'Germany')*100), '%') FROM world WHERE continent = 'Europe';
+
+6.
+SELECT name FROM world
+WHERE gdp > ALL
+(SELECT gdp FROM world WHERE gdp > 0 AND continent = 'Europe');
+
+7.
+SELECT continent, name, area FROM world x
+  WHERE area >= ALL
+(SELECT area FROM world y WHERE x.continent = y.continent AND y.area>0);
+
+8.
+SELECT continent, name FROM world x
+WHERE name <= ALL(SELECT name FROM world y WHERE x.continent = y.continent);
+
+9.
+SELECT name, continent, population FROM world
+WHERE continent IN
+(SELECT continent FROM world  x
+WHERE 25000000 >= (SELECT MAX(population) FROM world y
+WHERE x.continent = y.continent));
+or
+SELECT y.name, y.continent, y.population
+FROM world AS y
+JOIN
+(SELECT continent,max(population) FROM world
+GROUP BY continent
+HAVING max(population) <= 25000000) AS x
+ON y.continent = x.continent
+
+10.
+SELECT name, continent FROM world x
+WHERE population > ALL(SELECT 3*population FROM world y WHERE x.continent = y.continent AND x.name <> y.name)
 
