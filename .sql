73c0081d8aaa6b6327a3a475c25c730bd95e6476
anapdh <ana_d_hubner@hotmail.com>
2021-01-07 15:38:33 -0300

    add practice and quizz 8

--- .sql
+++ .sql
@@ -707,3 +707,84 @@ SELECT title, yr FROM movie, casting, actor
 
   =========== Using Null ===========
 This tutorial introduces the notion of a join. The database consists of three tables movie , actor and casting.
+
+1.
+SELECT name
+FROM teacher
+WHERE dept IS NULL
+
+2.
+SELECT teacher.name, dept.name
+ FROM teacher INNER JOIN dept
+           ON (teacher.dept=dept.id)
+
+3.
+SELECT teacher.name, dept.name
+FROM teacher LEFT JOIN dept ON (teacher.dept=dept.id)
+
+4.
+SELECT teacher.name, dept.name
+FROM teacher RIGHT JOIN dept ON (teacher.dept=dept.id)
+
+5.
+SELECT name, COALESCE(mobile, '07986 444 2266')
+FROM teacher;
+
+6.
+-
+
+7.
+SELECT COUNT(name), COUNT(mobile)
+FROM teacher;
+
+8.
+SELECT dept.name ,COUNT(teacher.name)
+FROM teacher RIGHT OUTER JOIN dept ON teacher.dept = dept.id
+GROUP BY dept.name;
+
+9.
+SELECT teacher.name, 
+CASE WHEN teacher.dept IN (1, 2) THEN 'Sci'
+ELSE 'Art'
+END
+FROM teacher;
+
+10.
+SELECT teacher.name, 
+CASE WHEN teacher.dept IN (1, 2) THEN 'Sci'
+WHEN teacher.dept = 3 THEN 'Art'
+ELSE 'None'
+END
+FROM teacher;
+
+QUIZZ 8
+
+1.
+SELECT teacher.name, dept.name FROM teacher LEFT OUTER JOIN dept ON (teacher.dept = dept.id);
+
+2.
+SELECT dept.name FROM teacher JOIN dept ON (dept.id = teacher.dept) WHERE teacher.name = 'Cutflower';
+
+3.
+SELECT dept.name, COUNT(teacher.name) FROM teacher RIGHT JOIN dept ON dept.id = teacher.dept GROUP BY dept.name;
+
+4.
+SELECT name, dept, COALESCE(dept, 0) AS result FROM teacher on teacher;
+
+5.
+SELECT name,
+       CASE WHEN phone = 2752 THEN 'two'
+            WHEN phone = 2753 THEN 'three'
+            WHEN phone = 2754 THEN 'four'
+            END AS digit
+  FROM teacher;
+
+6.
+SELECT name, 
+      CASE 
+       WHEN dept 
+        IN (1) 
+        THEN 'Computing' 
+       ELSE 'Other' 
+      END 
+  FROM teacher;
